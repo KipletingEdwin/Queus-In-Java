dfdd28c7dae6509106458e57d8a6ea6fc1a71976
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-03 23:13:57 +0300

    add a Queue

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Queue   =  FIFO data structure
+        // A collection designed for holding elementd prior to processing
+        //Linear data structure
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //add   =    enqueue, offer()
+        //remove   =  dequeue, poll()
+
+        Queue<String> queue = new LinkedList<String>();
+
     }
 }
